7a5d2c340d6805e036d9bbcc298a17ddc211a4cc
UV Panta <uv.panta@sensand.com>
2024-06-23 00:41:27 +1000

    feat: generic version of toy robot simulator challenge using arrow keybindings

---
+++ toy-robot/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Game from './Game'
+
+function App() {
+
+  return (
+    <>
+      <Game />
+    </>
+  )
+}
+
+export default App
---
+++ toy-robot/src/Game.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+
+const person = "🚶";
+const BOARD_SIZE = 5;
+const Direction = {
+  NORTH: "NORTH",
+  SOUTH: "SOUTH",
+  EAST: "EAST",
+  WEST: "WEST"
+};
+
+interface SquareProps {
+    direction: string;
+    showPerson: boolean;
+}
+
+interface BoardProps {
+    direction: string;
+    position: number[];
+}
+
+function Square({ showPerson, direction }: SquareProps) {
+  return <div className={`${direction} square`}>{showPerson && person}</div>;
+}
+
+function Board({ direction, position }: BoardProps) {
+  return (
+    <>
+      {Array(5)
+        .fill(0)
+        .map((_, row) => (
+          <div className="board-row" key={row}>
+            {Array(5)
+              .fill(0)
+              .map((_, column) => (
+                <Square
+                  key={column}
+                  direction={direction}
+                  showPerson={row === position[0] && column === position[1]}
+                />
+              ))}
+          </div>
+        ))}
+    </>
+  );
+}
+
+export default function Game() {
+  const [position, setPosition] = useState([0, 0]);
+  const [direction, setDirection] = useState(Direction.WEST);
+
+  useEffect(() => {
+    const handler = (event: KeyboardEvent) => {
+      switch (event.code) {
+        case "ArrowRight":
+          setDirection(Direction.EAST);
+          setPosition((position) => {
+            if (position[1] === BOARD_SIZE - 1) return position;
+            return [position[0], position[1] + 1];
+          });
+
+          break;
+        case "ArrowLeft":
+          setDirection(Direction.WEST);
+          setPosition((position) => {
+            if (position[1] === 0) return position;
+            return [position[0], position[1] - 1];
+          });
+          break;
+        case "ArrowUp":
+          setDirection(Direction.NORTH);
+          setPosition((position) => {
+            if (position[0] === 0) return position;
+            return [position[0] - 1, position[1]];
+          });
+          break;
+        case "ArrowDown":
+          setDirection(Direction.SOUTH);
+          setPosition((position) => {
+            if (position[0] === BOARD_SIZE - 1) return position;
+            return [position[0] + 1, position[1]];
+          });
+          break;
+        default:
+      }
+    };
+
+    window.addEventListener("keydown", handler, false);
+
+    return () => window.removeEventListener("keydown", handler, false);
+  }, [setDirection]);
+
+  return (
+    <>
+      <Board direction={direction} position={position} />
+    </>
+  );
+}
